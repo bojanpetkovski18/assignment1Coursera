b181ded602c9f695b2d5a1425a2dbc59415d8e2c
Bojan Petkovski <petkovski.bojan18@gmail.com>
2021-03-25 12:47:56 +0100

    First Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'assignment-app-first';
+  public input!: string;
+  public message: string = '';
+
+  public splitedStringCount(str: string) {
+    return str.split(',').length;
+  }
+
+  public determineLength(str: string) {
+    if (str == undefined || str == '') this.message = 'Please enter data first';
+    else
+      this.message = this.splitedStringCount(str) <= 3 ? 'Enjoy!' : 'Too Much!';
+  }
 }
